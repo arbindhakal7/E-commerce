cfd103a7428fb4101f7299f3f8d99c0ab53b8c2b
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-18 12:17:38 +0545

    :sparkles: Project completed

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-              var swiper = new Swiper(".mySwiper", {
-                      effect: "coverflow",
-                      grabCursor: true,
-                      centeredSlides: true,
-                      slidesPerView: "auto",
-                      coverflowEffect: {
-                        rotate: 50,
-                        stretch: 0,
-                        depth: 1200,
-                        modifier: 1,
-                        slideShadows: false,
-                      },
-                      pagination: {
-                        el: ".swiper-pagination",
-                      },
-                    });
+            //   var swiper = new Swiper(".mySwiper", {
+            //           effect: "coverflow",
+            //           grabCursor: true,
+            //           centeredSlides: true,
+            //           slidesPerView: "auto",
+            //           coverflowEffect: {
+            //             rotate: 50,
+            //             stretch: 0,
+            //             depth: 1200,
+            //             modifier: 1,
+            //             slideShadows: false,
+            //           },
+            //           pagination: {
+            //             el: ".swiper-pagination",
+            //           },
+            //         });
+
+            const swiper = new Swiper('.swiper-container', {
+                effect: 'coverflow',
+                centeredSlides: true,
+                slidesPerView: 2,
+                loop: true,
+                speed: 600,
+                
+                 autoplay: {
+                    delay: 1000,
+                 },
+                
+                coverflowEffect: {
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 1000,
+                    modifier: 1,
+                    slideShadows: false,
+                },
+                
+                breakpoints: {
+                    1500: {
+                        slidesPerView: 2,
+                    }
+                },
+            
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            
+    
+            });
+            
